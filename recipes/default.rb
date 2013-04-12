0fdbd8814a0485e9ed7f590b4a7b4be392e34a1c
Nelson Chen <crazysim@gmail.com>
2013-04-12 12:21:46 -0700

    try notifying this way

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,19 +7,19 @@
 # All rights reserved - Do Not Redistribute
 #
 
-git '/home/vagrant/demo/' do
-  repository 'https://github.com/crazysim/nanoc-foundation-blog.git'
-  reference 'overflow-demo'
-  action :sync
-  notifies :run, 'script[demo-bundler]', :immediately
-  user 'vagrant'
-  group 'vagrant'
-end
-
-script 'demo-bundler' do
-  interpreter 'zsh'
+bash 'demo_bundler' do
   cwd '/home/vagrant/demo'
   user 'vagrant'
   code 'bundle'
   action :nothing
 end
+
+git '/home/vagrant/demo/' do
+  repository 'https://github.com/crazysim/nanoc-foundation-blog.git'
+  reference 'overflow-demo'
+  action :sync
+  user 'vagrant'
+  group 'vagrant'
+  notifies :run, 'bash[demo_bundler]', :immediately
+end
+
